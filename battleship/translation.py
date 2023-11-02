693d7d7463fb227f39f2ae3e52472e4d73656cbf
Gabe Grand <grandg@mit.edu>
2023-11-02 16:09:00 -0400

    Add translation tests

--- battleship/translation.py
+++ battleship/translation.py
@@ -4,12 +4,20 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
 class Translator(object):
-    def __init__(self, model_name: str = "bigcode/starcoder"):
+    def __init__(
+            self,
+            model_name: str = "Salesforce/codegen-350M-nl",
+            max_new_tokens: int = 128,
+        ):
         hf_auth_token = os.environ.get("HF_AUTH_TOKEN")
+
+        self.model_name = model_name
+        self.max_new_tokens = max_new_tokens
+
         self.tokenizer = AutoTokenizer.from_pretrained(model_name, token=hf_auth_token)
         self.model = AutoModelForCausalLM.from_pretrained(model_name, token=hf_auth_token)
 
     def translate(self, text: str) -> str:
-        inputs = self.tokenizer(text, return_tensors="pt", max_length=512)
-        outputs = self.model.generate(**inputs)
+        inputs = self.tokenizer(text, return_tensors="pt")
+        outputs = self.model.generate(**inputs, max_new_tokens=self.max_new_tokens)
         return self.tokenizer.decode(outputs[0], skip_special_tokens=True)
---
+++ tests/test_translation.py
@@ -0,0 +1,6 @@
+from battleship.translation import Translator
+
+def test_translate():
+    translator = Translator()
+    translation = translator.translate("def add_one(x):")
+    assert(isinstance(translation, str))
