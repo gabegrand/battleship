93e97112de11dbc9b7867753a571e88375c90372
Gabe Grand <grandg@mit.edu>
2023-12-06 16:06:37 +0000

    switch scoring to use chunks

--- battleship/scoring.py
+++ battleship/scoring.py
@@ -1,3 +1,4 @@
+import itertools
 import os
 from multiprocessing import Pool
 
@@ -30,20 +31,40 @@ def compute_score(program: str, board: Board):
     return np.round(score, decimals=6)
 
 
+def _compute_score_batch(programs: list, board: Board, show_progress: bool = True):
+    """Compute scores for a list of programs."""
+
+    scores = []
+    for program in tqdm(programs) if show_progress else programs:
+        scores.append(compute_score(program, board))
+
+    return scores
+
+
 def compute_score_parallel(
-    programs: list, board: Board, processes: int = None, show_progress: bool = True
+    programs: list,
+    board: Board,
+    processes: int = None,
+    n_chunks_per_process: int = 4,
+    show_progress: bool = True,
 ):
     """Compute scores for a list of programs in parallel."""
 
     if processes is None:
         processes = os.cpu_count()
 
+    # Split programs into chunks using np.array_split
+    program_chunks = np.array_split(programs, int(processes * n_chunks_per_process))
+
     with Pool(processes=processes) as pool:
         job_list = [
-            pool.apply_async(compute_score, (program, board)) for program in programs
+            pool.apply_async(_compute_score_batch, (chunk, board, False))
+            for chunk in program_chunks
         ]
         if show_progress:
             job_list = tqdm(job_list)
-        scores = [job.get() for job in job_list]
+        scores_chunks = [job.get() for job in job_list]
+
+    scores = list(itertools.chain.from_iterable(scores_chunks))
 
     return scores
