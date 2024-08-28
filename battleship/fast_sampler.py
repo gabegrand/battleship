349c48395d5638e13ac784416cda3040ef40981b
Gabe Grand <grandg@mit.edu>
2024-08-28 15:37:02 -0400

    refactoring changes

--- battleship/fast_sampler.py
+++ battleship/fast_sampler.py
@@ -1,3 +1,7 @@
+"""Fast conditional sampler for generating random Battleship boards.
+
+Author: Gabe Grand (grandg@mit.edu)
+"""
 from collections import defaultdict
 from enum import StrEnum
 from typing import List
@@ -81,16 +85,16 @@ class FastSampler:
         """Computes a data structure that stores all possible spans for ships with the specified lengths."""
 
         # Stores all unique spans
-        self.spans = set()
+        self._spans = set()
 
         # Maps span_id to Span object
-        self.spans_by_id = {}
+        self._spans_by_id = {}
 
         # Maps ship_length to Span objects
-        self.spans_by_length = defaultdict(set)
+        self._spans_by_length = defaultdict(set)
 
         # Maps tile to span_id of all spans that contain that tile
-        self.spans_by_tile = defaultdict(set)
+        self._spans_by_tile = defaultdict(set)
 
         for length in self.ship_lengths:
             # Add vertical spans
@@ -98,36 +102,34 @@ class FastSampler:
                 for j in range(self.board.size):
                     topleft = (i, j)
                     bottomright = (i + length - 1, j)
-                    span = Span(len(self.spans), topleft, bottomright)
-                    self.spans.add(span)
-                    self.spans_by_id[span.id] = span
-                    self.spans_by_length[length].add(span.id)
-
-                    for tile in span.tiles:
-                        self.spans_by_tile[tile].add(span.id)
+                    span = Span(len(self._spans), topleft, bottomright)
+                    self._add_span(span, length)
 
             # Add horizontal spans
             for i in range(self.board.size):
                 for j in range(self.board.size - length + 1):
                     topleft = (i, j)
                     bottomright = (i, j + length - 1)
-                    span = Span(len(self.spans), topleft, bottomright)
-                    self.spans.add(span)
-                    self.spans_by_id[span.id] = span
-                    self.spans_by_length[length].add(span.id)
+                    span = Span(len(self._spans), topleft, bottomright)
+                    self._add_span(span, length)
 
-                    for tile in span.tiles:
-                        self.spans_by_tile[tile].add(span.id)
+    def _add_span(self, span, length):
+        self._spans.add(span)
+        self._spans_by_id[span.id] = span
+        self._spans_by_length[length].add(span.id)
+
+        for tile in span.tiles:
+            self._spans_by_tile[tile].add(span.id)
 
     def complete_board(self):
         """Randomly places all ships on the board, ensuring that ships are placed in unoccupied spans."""
         new_board = self.board.board.copy()
 
-        available_span_ids_global = set(self.spans_by_id.keys())
+        available_span_ids_global = set(self._spans_by_id.keys())
 
         # Discard all spans that contain a water tile (0)
         for tile in zip(*np.where(new_board == 0)):
-            for span_id in self.spans_by_tile[tile]:
+            for span_id in self._spans_by_tile[tile]:
                 available_span_ids_global.discard(span_id)
 
         for ship_length, ship_label in zip(self.ship_lengths, self.ship_labels):
@@ -136,7 +138,7 @@ class FastSampler:
 
             # Start with all spans matching the ship's length
             available_span_ids = available_span_ids_global.intersection(
-                self.spans_by_length[ship_length]
+                self._spans_by_length[ship_length]
             )
 
             # If the ship is already (partially) placed on the board, discard all spans that do not contain the ship
@@ -144,7 +146,7 @@ class FastSampler:
                 # Find all spans that contain all of the ship's visible tiles
                 ship_tiles = list(zip(*np.where(new_board == ship_id)))
                 ship_span_ids = set.intersection(
-                    *[self.spans_by_tile[tile] for tile in ship_tiles]
+                    *[self._spans_by_tile[tile] for tile in ship_tiles]
                 )
 
                 # Restrict the set of possible spans for the ship
@@ -156,14 +158,14 @@ class FastSampler:
 
             # Randomly select a span to place the ship
             span_id = self.rng.choice(list(available_span_ids))
-            span = self.spans_by_id[span_id]
+            span = self._spans_by_id[span_id]
 
             for tile in span.tiles:
                 # Add the ship to the board
                 new_board[tile] = ship_id
 
                 # Discard all spans that contain this tile
-                for span_id in self.spans_by_tile[tile]:
+                for span_id in self._spans_by_tile[tile]:
                     available_span_ids_global.discard(span_id)
 
         return Board(new_board)
